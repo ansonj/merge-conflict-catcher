e4ad4aa23bf02e8696fc3ed86859c4d167f63b79
Anson Jablinski <a@ansonj.org>
2018-08-08 10:21:13 -0500

    Warn for merges that take longer than expected
    - These branches probably haven't been updated in a while.

--- checkForMergeConflicts.swift
+++ checkForMergeConflicts.swift
@@ -6,6 +6,7 @@ import Foundation
 
 let remote = "origin"
 let defaultBranch = "master"
+let mergeTimeWarningUpperBound: TimeInterval = 3.0
 
 struct Branch: Hashable {
     let name: String
@@ -27,6 +28,7 @@ enum LogColor: Int {
     case black = 0
     case red = 31
     case green = 32
+    case yellow = 33
     case blue = 34
     
     var terminalString: String {
@@ -67,7 +69,13 @@ func run(gitCommand args: String, andFailWithDescriptionIfNeeded description: St
     }
 }
 
-var mergeSucceeded = [Branch : Bool]()
+enum MergeResult {
+    case succeeded
+    case tookALongTime
+    case failed
+}
+
+var mergeResults = [Branch : MergeResult]()
 
 run(gitCommand: "fetch --jobs=5 --recurse-submodules", andFailWithDescriptionIfNeeded: "Fetch")
 
@@ -77,16 +85,19 @@ for branch in branches {
     let mergeCompletionInfo = run(gitCommand: "merge --no-edit origin/\(branch.target)")
     if mergeCompletionInfo.exitStatus != 0 {
         log("Merge conflict detected: \(branch.name) --> \(branch.target)", color: .red)
-        mergeSucceeded[branch] = false
+        mergeResults[branch] = .failed
         run(gitCommand: "merge --abort", andFailWithDescriptionIfNeeded: "Merge abort")
+    } else if mergeCompletionInfo.executionTime > mergeTimeWarningUpperBound {
+        log("Merge took \(mergeCompletionInfo.executionTime) seconds, exceeding warning threshold of \(mergeTimeWarningUpperBound) seconds", color: .yellow)
+        mergeResults[branch] = .tookALongTime
     } else {
-        mergeSucceeded[branch] = true
+        mergeResults[branch] = .succeeded
     }
     testsComplete += 1
     log("Tested \(testsComplete) of \(branches.count) merges...")
 }
 
-let firstBranchWithConflicts: Branch? = branches.filter { mergeSucceeded[$0] == false }.first
+let firstBranchWithConflicts: Branch? = branches.filter { mergeResults[$0] == .failed }.first
 let branchToResetTo = firstBranchWithConflicts?.name ?? defaultBranch
 run(gitCommand: "checkout \(branchToResetTo)", andFailWithDescriptionIfNeeded: "Checkout \(branchToResetTo)")
 run(gitCommand: "submodule update --init --recursive", andFailWithDescriptionIfNeeded: "Submodule update")
@@ -96,10 +107,14 @@ log("Results:")
 for branch in branches {
     let color: LogColor
     let glyph: String
-    if mergeSucceeded[branch, default: false] {
+    switch mergeResults[branch, default: .failed] {
+    case .succeeded:
         color = .green
         glyph = "\u{2705}"
-    } else {
+    case .tookALongTime:
+        color = .yellow
+        glyph = "\u{26A0}"
+    case .failed:
         color = .red
         glyph = "\u{274C}"
     }
@@ -107,13 +122,22 @@ for branch in branches {
 }
 log("")
 
+let printStaleBranchWarning: () -> () = {
+    let slowMergeCount = mergeResults.values.filter { $0 == .tookALongTime }.count
+    if slowMergeCount > 0 {
+        log("At least one merge took longer than \(mergeTimeWarningUpperBound) seconds; consider updating these branches with the latest from their respective parent branches.", color: .yellow)
+    }
+}
+
 if let firstBranchWithConflicts = firstBranchWithConflicts {
-    let conflictCount = branches.filter { mergeSucceeded[$0] == false }.count
+    let conflictCount = branches.filter { mergeResults[$0] == .failed }.count
     log("Found \(conflictCount) conflict\(conflictCount == 1 ? "" : "s").", color: .red)
+    printStaleBranchWarning()
     log("")
     log("Checked out the first conflict for you to fix: \(LogColor.red.terminalString)\(firstBranchWithConflicts.name)\(LogColor.blue.terminalString) --> \(firstBranchWithConflicts.target)")
 } else {
     log("No conflicts detected.")
+    printStaleBranchWarning()
     log("")
     log("Returned to \(defaultBranch) (your default branch).")
 }
